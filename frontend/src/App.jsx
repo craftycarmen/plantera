0ab310508cecfd012c755e099c33896c0b9b24c1
Carmen <carmenshiu07@gmail.com>
2024-04-08 17:08:31 -0700

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      }
+    ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
+// ** WITHOUT LOGIN/SIGNUP MODALS ** //
+// import { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import LoginFormPage from './components/LoginFormPage';
+// import SignupFormPage from './components/SignupFormPage';
+// import Navigation from './components/Navigation';
+// import * as sessionActions from './store/session';
+
+// function Layout() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => {
+//       setIsLoaded(true)
+//     });
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <Navigation isLoaded={isLoaded} />
+//       {isLoaded && <Outlet />}
+//     </>
+//   );
+// }
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <h1>Welcome!</h1>
+//       },
+//       {
+//         path: "login",
+//         element: <LoginFormPage />
+//       },
+//       {
+//         path: "signup",
+//         element: <SignupFormPage />
+//       }
+//     ]
+//   }
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,81 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const res = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    });
+
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+};
+
+export const restoreUser = () => async (dispatch) => {
+    const res = await csrfFetch("/api/session");
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+};
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+
+    const res = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    })
+
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+}
+
+export const logout = () => async (dispatch) => {
+    const res = await csrfFetch('/api/session', {
+        method: "DELETE"
+    });
+    dispatch(removeUser());
+    return res;
+}
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, user: action.payload };
+        case REMOVE_USER:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;

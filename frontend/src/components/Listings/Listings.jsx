785897fec5821fd9bf85cd9f8ce877b26cb951d6
Carmen <carmenshiu07@gmail.com>
2024-04-10 20:33:15 -0700

    finished v1 of listings page frontend

--- frontend/src/components/Listings/Listings.jsx
+++ frontend/src/components/Listings/Listings.jsx
@@ -17,17 +17,24 @@ function Listings() {
     return (listings &&
         <>
             <h1>Shop</h1>
+            <div>Shop for plants from fellow plant lovers!</div>
+            <br />
             <div className="listingsContainer">
                 {
                     listings.map((listing) => (
                         <div key={listing.id}>
-                            <img
-                                className="listingImage"
-                                src={listing.ListingImages[0].url} />
-                            <div>
-                                <h2>{listing.plantName}</h2>
-                                <div>${listing.price}</div>
+                            <div className="listingImageContainer">
+                                <img
+                                    className="listingImage"
+                                    src={listing.ListingImages[0].url} />
                             </div>
+                            <div className="listingNamePrice">
+                                <h2>{listing.plantName}</h2>
+                                <span>${listing.price}</span>
+                            </div>
+                            <div>from {listing.Seller?.username}
+                            </div>
+
                         </div>
                     ))
                 }

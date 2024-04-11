9bf0627dbfec1da36f7dd3cb3f993162fc35d37a
Carmen <carmenshiu07@gmail.com>
2024-04-10 17:55:15 -0700

    added listingguides model

---
+++ backend/db/seeders/20240411005205-listingguides.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { ListingGuide } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const listingGuidesList = [
+  {
+    listingId: 1,
+    guideId: 1
+  },
+  {
+    listingId: 2,
+    guideId: 3
+  }
+]
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await ListingGuide.bulkCreate(listingGuidesList, { validate: true })
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'ListingGuides';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, { [Op.or]: listingGuidesList }, {});
+  }
+};

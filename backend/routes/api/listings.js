0d0c846c919b9e15e4c0fefef9a47004a81bd3bc
Carmen <carmenshiu07@gmail.com>
2024-04-11 16:14:32 -0700

    added get current listings be

--- backend/routes/api/listings.js
+++ backend/routes/api/listings.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { Listing, Image, User, Guide, ListingGuide } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
@@ -40,6 +41,63 @@ router.get('/', async (req, res) => {
 
     return res.json({ Listings: listingsList });
 
+});
+
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req;
+
+    if (user) {
+        const listings = await Listing.findAll({
+            include: [
+                {
+                    model: Image,
+                    as: 'ListingImages',
+                    attributes: {
+                        exclude: ['avatar']
+                    }
+                },
+                {
+                    model: Guide,
+                    through: {
+                        model: ListingGuide,
+                    },
+                    attributes: ['id', 'title', 'userId'],
+                    include: [
+                        {
+                            model: Image,
+                            as: 'GuideImages',
+                            attributes: {
+                                exclude: ['avatar']
+                            }
+                        }
+                    ]
+                }
+            ],
+            where: {
+                sellerId: user.id
+            }
+        })
+
+
+        let listingsList = [];
+
+        listings.forEach(listing => {
+            listingsList.push(listing.toJSON());
+        });
+
+        listingsList.forEach(listing => {
+            console.log(listing.Guides);
+            if (listing.Guides.length === 0) {
+                listing.Guides = null
+            }
+        });
+
+        if (listingsList.length === 0) {
+            return res.json({ Listings: "No listings found" })
+        } else {
+            return res.json({ Listings: listingsList });
+        }
+    }
 });
 
 router.get('/:listingId', async (req, res) => {
@@ -91,6 +149,6 @@ router.get('/:listingId', async (req, res) => {
             message: "Listing couldn't be found"
         });
     }
-})
+});
 
 module.exports = router;

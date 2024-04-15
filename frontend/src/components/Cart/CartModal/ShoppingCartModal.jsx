e440b42558283994478b49c836b3d107f2c2111b
Carmen <carmenshiu07@gmail.com>
2024-04-15 15:52:30 -0700

    added shells for cart modal

---
+++ frontend/src/components/Cart/CartModal/ShoppingCartModal.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useCartModal } from "../../../context/CartModal/Modal";
+
+function ShoppingCartModal() {
+    const dispatch = useDispatch();
+}
+
+export default ShoppingCartModal;

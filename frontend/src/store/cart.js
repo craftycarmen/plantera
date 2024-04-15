3a0a84be21810dbb9e642677d1a883dc0acb4ec0
Carmen <carmenshiu07@gmail.com>
2024-04-15 16:11:20 -0700

    added loadCart thunk action creator

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,11 +1,29 @@
 // import { csrfFetch } from "./csrf";
 
+const LOAD_CART = 'cart/LOAD_CART';
 const CREATE_CART = 'cart/CREATE_CART';
 
+export const loadCart = (cart) => ({
+    type: LOAD_CART,
+    cart
+})
 export const createCart = (cart) => ({
     type: CREATE_CART,
     cart
 });
+
+export const fetchCart = (cartId) => async (dispatch) => {
+    const res = await fetch(`/api/cart/${cartId}`);
+
+    if (res.ok) {
+        const cart = await res.json();
+        dispatch(loadCart(cart));
+        return cart
+    } else {
+        const errors = await res.json();
+        return errors;
+    }
+}
 
 export const addCart = (cart) => async (dispatch) => {
     const res = await ('/api/cart', {
@@ -25,6 +43,9 @@ export const addCart = (cart) => async (dispatch) => {
 
 const cartReducer = (state = {}, action) => {
     switch (action.type) {
+        case LOAD_CART: {
+            return { ...state, [action.cart.id]: action.cart }
+        }
         case CREATE_CART: {
             return { ...state, [action.cart.id]: action.cart }
         }

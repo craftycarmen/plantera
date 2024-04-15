d2db2b4ea6b27e6ea5f03922e139d8e1f0b4ea39
Carmen <carmenshiu07@gmail.com>
2024-04-14 23:40:39 -0700

    added component

---
+++ frontend/src/components/ErrorHandling/ErrorHandling.jsx
@@ -0,0 +1,10 @@
+function ErrorHandling() {
+    return (
+        <>
+            <div>To access this page, you must log in or sign up for Plantera.</div>
+            <div><img src='../../ohno.png' /></div>
+        </>
+    )
+}
+
+export default ErrorHandling

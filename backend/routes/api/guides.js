a40de1a6d77a1b461288485129df1207ac552391
Carmen <carmenshiu07@gmail.com>
2024-05-10 21:25:21 -0700

    get guide by id be

--- backend/routes/api/guides.js
+++ backend/routes/api/guides.js
@@ -30,6 +30,33 @@ router.get('/', async (req, res) => {
     });
 
     return res.json({ Guides: guidesList })
+});
+
+router.get('/:guideId', async (req, res) => {
+    const guide = await Guide.findByPk(req.params.guideId, {
+        include: [
+            {
+                model: Image,
+                as: 'GuideImages',
+                attributes: {
+                    exclude: ['avatar']
+                }
+            },
+            {
+                model: User,
+                as: 'Author',
+                attributes: ['id', 'username']
+            }
+        ]
+    })
+
+    if (guide) {
+        return res.json(guide)
+    } else {
+        return res.status(404).json({
+            message: "Guide couldn't be found"
+        })
+    }
 })
 
 module.exports = router;

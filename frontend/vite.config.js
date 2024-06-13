f007fbb613f113c6eb7e111454b03e0f147f53d4
Carmen <carmenshiu07@gmail.com>
2024-06-13 10:28:16 -0700

    attempt no 13

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import commonjs from 'vite-plugin-commonjs';
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => ({
       lintOnStart: true,
       failOnError: mode === "production"
     }),
+    vitePluginRequire.default()
   ],
   server: {
     proxy: {

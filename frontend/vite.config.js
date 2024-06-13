1081a1ded85df01aeed51ba3bbf62d205effd1a5
Carmen <carmenshiu07@gmail.com>
2024-06-12 17:19:49 -0700

    attempt no7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-import commonjs from 'vite-plugin-commonjs';
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => ({
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        esbuildCommonjs(['react-quilljs'])
+      ]
+    }
+  },
   plugins: [
     react(),
     eslint({
       lintOnStart: true,
       failOnError: mode === "production"
-    }),
-    commonjs({
-      filter(id) {
-        if (id.includes('node_modules/react-quilljs/build-es')) {
-          return true;
-        }
-      },
     }),
   ],
   server: {

3522062918d033c74b0db76ae05731afa12de8be
Carmen <carmenshiu07@gmail.com>
2024-10-07 17:24:27 -0700

    ran build

--- frontend/src/components/Cart/CartModal/ShoppingCartModal.jsx
+++ frontend/src/components/Cart/CartModal/ShoppingCartModal.jsx
@@ -14,7 +14,7 @@ function ShoppingCartModal({ cartId, navigate, updatedQty }) {
     // const cartItemsLocalStorage = JSON.parse(localStorage.getItem('cartItems')) || [];
     const sessionUser = useSelector(state => state.session.user);
     const userId = sessionUser?.id;
-    const [loading, setLoading] = useState(true);
+    const [loading] = useState(true);
     // 
 
 
